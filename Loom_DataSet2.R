1626c0f5cbb515825764c7bbc18e66ffee2e4993
Trent Simmons <trentneuro42@gmail.com>
2022-10-03 17:33:55 -0400

    analysis 2 changes

--- Loom_DataSet2.R
+++ Loom_DataSet2.R
@@ -1,4 +1,9 @@
-originalDF <- read.csv("analytics2.csv", header = TRUE, sep = ",")
+library(plot3D)
+library(rgl)
+
+
+participantDataFile <- "analytics2_P29.csv"
+originalDF <- read.csv(participantDataFile, header = TRUE, sep = ",")
 
 originalDF <-  originalDF[originalDF$xPos != "c",]
 
@@ -23,223 +28,260 @@ zCo <- coDF[ ,11]
 plot3d(xCo, yCo, zCo)
 
 
-#Set up the new PAC1 and PAC2 DF's
-soloDFshort <- data.frame(TimeStamp = numeric(),
-                   Participant = factor(),
-                   Condition = factor(),
-                   Trial = numeric(),
-                   Age = numeric(),
-                   Gender = factor(),
-                   SessionTime = numeric(),
-                   Event = factor(),
-                   xPos = numeric(),
-                   yPos = numeric(),
-                   zPos = numeric(),
-                   group = factor(),
-                   stringsAsFactors = FALSE)
 
-
-
-#Setup Variables
-lookingForViewWall <- FALSE
-lookingForBuildWall <- FALSE
-lookingForPlayWall <- FALSE
-lookingForNewSeq <- TRUE
-
-viewWall <- "looking at View wall"
-viewWall <- toString(viewWall)
-
-playWall <- "looking at Play wall"
-playWall <- toString(playWall)
-
-buildWall <- "looking at Build wall"
-buildWall <- toString(buildWall)
-
-
-#this loop is picking out all of the row that are needed for analysis and putting them into their DF's
-
-for (i in 2:nrow(soloDF))
+if(participantDataFile == "analytics2_P2.csv" | participantDataFile == "analytics2_P3.csv" | participantDataFile == "analytics2_P4.csv" | participantDataFile == "analytics2_P6.csv" |
+   participantDataFile == "analytics2_P7.csv" | participantDataFile == "analytics2_P8.csv" | participantDataFile == "analytics2_P10.csv" | participantDataFile == "analytics2_P11.csv" |
+   participantDataFile == "analytics2_P12.csv" | participantDataFile == "analytics2_P14.csv" | participantDataFile == "analytics2_P16.csv")
 {
-  currentEvent <- soloDF[i,8]
-  currentEvent <- toString(currentEvent)
-  #print(currentEvent)
-  
-  currentCondition <- soloDF[i,3]
-  currentCondition <- toString(currentCondition)
-  
+  partGroup <- "e"
+}else{
+  partGroup <- "c"
+}
 
-  if(lookingForNewSeq == FALSE){
-      if(lookingForPlayWall){
-        if(currentEvent == viewWall | currentEvent == buildWall){
-          second_row_to_add <- soloDF[i,]
-          #soloDFshort <- rbind(soloDFshort, first_row_to_add)
-          soloDFshort <- rbind(soloDFshort, second_row_to_add)
-          #print("added a new Blue sequence")
-          lookingForPlayWall <- FALSE
-          lookingForNewSeq <- TRUE
-          
-        }else if(currentEvent == playWall){
-          #print("continue looking at blue")
-        }else{
-          #print("bad blue")
-          lookingForPlayWall <- FALSE
-          lookingForNewSeq <- TRUE
-        }
-      }
-      if(lookingForBuildWall){
-        if(currentEvent == viewWall | currentEvent == playWall){
-          second_row_to_add <- soloDF[i,]
-          #soloDFshort <- rbind(soloDFshort, first_row_to_add)
-          soloDFshort <- rbind(soloDFshort, second_row_to_add)
-          #print("added a new Red sequence")
-          lookingForBuildWall <- FALSE
-          lookingForNewSeq <- TRUE
-          
-        }else if(currentEvent == buildWall){
-          #print("continue looking at Red")
-        }else{
-          #print("bad red")
-          
-          lookingForBuildWall <- FALSE
-          lookingForNewSeq <- TRUE
-        }
-      }
-      if(lookingForViewWall){
-        if(currentEvent == buildWall | currentEvent == playWall){
-          second_row_to_add <- soloDF[i,]
-          #soloDFshort <- rbind(soloDFshort, first_row_to_add)
-          soloDFshort <- rbind(soloDFshort, second_row_to_add)
-          #print("added a new Invis sequence")
-          lookingForViewWall <- FALSE
-          lookingForNewSeq <- TRUE
-          
-        }else if(currentEvent == viewWall){
-          #print("continue looking at Invis")
-        }else{
-          #print("bad invis")
-          
-          lookingForViewWall <- FALSE
-          lookingForNewSeq <- TRUE
-        }
-      }
+groupCounter <- 0
+for(j in 0:1)
+{
+  shortGroupDF <- data.frame(TimeStamp = numeric(),
+                             Participant = factor(),
+                             Condition = factor(),
+                             Trial = numeric(),
+                             Age = numeric(),
+                             Gender = factor(),
+                             SessionTime = numeric(),
+                             Event = factor(),
+                             xPos = numeric(),
+                             yPos = numeric(),
+                             zPos = numeric(),
+                             group = factor(),
+                             stringsAsFactors = FALSE)
+  
+  if(j == 0){
+    groupDF <- soloDF
+    partCondition <- "s"
+  }else{
+    groupDF <- coDF
+    partCondition <- "co"
   }
-  if(lookingForNewSeq){
+
+  
+  #Setup Variables
+  lookingForViewWall <- FALSE
+  lookingForBuildWall <- FALSE
+  lookingForPlayWall <- FALSE
+  lookingForNewSeq <- TRUE
+  
+  viewWall <- "looking at View wall"
+  viewWall <- toString(viewWall)
+  
+  playWall <- "looking at Play wall"
+  playWall <- toString(playWall)
+  
+  buildWall <- "looking at Build wall"
+  buildWall <- toString(buildWall)
+  
+  
+  #this loop is picking out all of the row that are needed for analysis and putting them into their DF's
+  for (i in 2:nrow(groupDF))
+  {
+    currentEvent <- groupDF[i,8]
+    currentEvent <- toString(currentEvent)
+    #print(currentEvent)
+    
+    currentCondition <- groupDF[i,3]
+    currentCondition <- toString(currentCondition)
+    
+  
+    if(lookingForNewSeq == FALSE){
+        if(lookingForPlayWall){
+          if(currentEvent == viewWall | currentEvent == buildWall){
+            second_row_to_add <- groupDF[i,]
+            #soloDFshort <- rbind(soloDFshort, first_row_to_add)
+            shortGroupDF <- rbind(shortGroupDF, second_row_to_add)
+            #print("added a new Blue sequence")
+            lookingForPlayWall <- FALSE
+            lookingForNewSeq <- TRUE
+            
+          }else if(currentEvent == playWall){
+            #print("continue looking at blue")
+          }else{
+            #print("bad blue")
+            lookingForPlayWall <- FALSE
+            lookingForNewSeq <- TRUE
+          }
+        }
+        if(lookingForBuildWall){
+          if(currentEvent == viewWall | currentEvent == playWall){
+            second_row_to_add <- groupDF[i,]
+            #soloDFshort <- rbind(soloDFshort, first_row_to_add)
+            shortGroupDF <- rbind(shortGroupDF, second_row_to_add)
+            #print("added a new Red sequence")
+            lookingForBuildWall <- FALSE
+            lookingForNewSeq <- TRUE
+            
+          }else if(currentEvent == buildWall){
+            #print("continue looking at Red")
+          }else{
+            #print("bad red")
+            
+            lookingForBuildWall <- FALSE
+            lookingForNewSeq <- TRUE
+          }
+        }
+        if(lookingForViewWall){
+          if(currentEvent == buildWall | currentEvent == playWall){
+            second_row_to_add <- groupDF[i,]
+            #soloDFshort <- rbind(soloDFshort, first_row_to_add)
+            shortGroupDF <- rbind(shortGroupDF, second_row_to_add)
+            #print("added a new Invis sequence")
+            lookingForViewWall <- FALSE
+            lookingForNewSeq <- TRUE
+            
+          }else if(currentEvent == viewWall){
+            #print("continue looking at Invis")
+          }else{
+            #print("bad invis")
+            
+            lookingForViewWall <- FALSE
+            lookingForNewSeq <- TRUE
+          }
+        }
+    }
+    if(lookingForNewSeq){
+      if(currentEvent == viewWall){
+        first_row_to_add <- groupDF[i,]
+        lookingForNewSeq <- FALSE
+        lookingForViewWall <- TRUE
+      }
+      if(currentEvent == playWall){
+        first_row_to_add <- groupDF[i,]
+        lookingForNewSeq <- FALSE
+        lookingForPlayWall <- TRUE
+      }
+      if(currentEvent == buildWall){
+        first_row_to_add <- groupDF[i,]
+        lookingForNewSeq <- FALSE
+        lookingForBuildWall <- TRUE
+      }
+    }
+    
+  }
+  
+  previousEvent <- ""
+  
+  previousTime <- 0
+  counter <- (-1) # -1 because the first iteration doesn't count 
+  
+  
+  totalViewWallCount <- 0
+  totalPlayWallCount <- 0
+  totalBuildWallCount <- 0
+  
+  totalTime <- 0
+  totalPlay2View <- 0
+  totalPlay2Build <- 0
+  totalBuild2Play <- 0
+  totalView2Play <- 0
+  
+  avgTotalTransferTime <- 0
+  avgPlay2View <- 0
+  avgPlay2Build <- 0
+  avgBuild2Play <- 0
+  avgView2Play <- 0
+  
+  play2ViewCounter <- 0
+  play2BuildCounter <- 0
+  build2PlayCounter <- 0
+  view2PlayCounter <- 0
+  
+  
+  for (i in 2:nrow(shortGroupDF))
+  {
+    counter <- counter +1
+    currentEvent <- shortGroupDF[i,8]
+    currentEvent <- toString(currentEvent)
+    
+    currentTime <- shortGroupDF[i,1]/10000
+    if(previousTime != 0){
+      previousTime
+      reactionTime <- currentTime - previousTime
+    }
+    
     if(currentEvent == viewWall){
-      first_row_to_add <- soloDF[i,]
-      lookingForNewSeq <- FALSE
-      lookingForViewWall <- TRUE
-      print("looking for View wall")
+      if(previousEvent == playWall){
+        
+        totalViewWallCount <- totalViewWallCount + 1
+        totalTime <- totalTime + reactionTime 
+        totalPlay2View <- totalPlay2View + reactionTime
+        play2ViewCounter <- play2ViewCounter + 1
+        
+        }else{
+        #print("nothin bb")
+      }
     }
-    if(currentEvent == playWall){
-      first_row_to_add <- soloDF[i,]
-      lookingForNewSeq <- FALSE
-      lookingForPlayWall <- TRUE
-      print("looking for Play wall")
-    }
+    
     if(currentEvent == buildWall){
-      first_row_to_add <- soloDF[i,]
-      lookingForNewSeq <- FALSE
-      lookingForBuildWall <- TRUE
-      print("looking for Build wall")
-    }
-  }
+      if(previousEvent == playWall){
+        
+        totalBuildWallCount <- totalBuildWallCount + 1
+        totalTime <- totalTime + reactionTime 
+        totalPlay2Build <- totalPlay2Build + reactionTime
+        play2BuildCounter <- play2BuildCounter + 1
   
-}
-
-previousEvent <- ""
-
-previousTime <- 0
-counter <- (-1) # -1 because the first itteration doesnt count 
-
-
-totalViewWallCount <- 0
-totalPlayWallCount <- 0
-totalBuildWallCount <- 0
-
-totalTime <- 0
-totalPlay2View <- 0
-totalPlay2Build <- 0
-totalBuild2Play <- 0
-totalView2Play <- 0
-
-avgTotalTransferTime <- 0
-avgPlay2View <- 0
-avgPlay2Build <- 0
-avgBuild2Play <- 0
-avgView2Play <- 0
-
-play2ViewCounter <- 0
-play2BuildCounter <- 0
-build2PlayCounter <- 0
-view2PlayCounter <- 0
-
-
-for (i in 2:nrow(soloDFshort))
-{
-  counter <- counter +1
-  currentEvent <- soloDFshort[i,8]
-  currentEvent <- toString(currentEvent)
-  
-  currentTime <- soloDFshort[i,1]/10000
-  if(previousTime != 0){
-    reactionTime <- currentTime - previousTime
-  }
-  
-  if(currentEvent == viewWall){
-    if(previousEvent == playWall){
-      
-      totalViewWallCount <- totalViewWallCount + 1
-      totalTime <- totalTime + reactionTime 
-      totalPlay2View <- totalPlay2View + reactionTime
-      play2ViewCounter <- play2ViewCounter + 1
-      
       }else{
-      print("nothin bb")
+        #print("nothin bb")
+      }
     }
+    
+    if(currentEvent == playWall){
+      if(previousEvent == viewWall){
+        
+        totalPlayWallCount <- totalPlayWallCount + 1
+        totalTime <- totalTime + reactionTime 
+        totalView2Play <- totalView2Play + reactionTime
+        view2PlayCounter <- view2PlayCounter + 1
+        
+      }else if(previousEvent == buildWall){
+        
+        totalPlayWallCount <- totalPlayWallCount + 1
+        totalTime <- totalTime + reactionTime 
+        totalBuild2Play <- totalBuild2Play + reactionTime
+        build2PlayCounter <- build2PlayCounter + 1
+        
+      }else{
+        #print("nothin bb")
+      }
+    }
+    previousEvent <- currentEvent
+    previousTime <- shortGroupDF[i,1]/10000
+    
+    
   }
   
-  if(currentEvent == buildWall){
-    if(previousEvent == playWall){
-      
-      totalBuildWallCount <- totalBuildWallCount + 1
-      totalTime <- totalTime + reactionTime 
-      totalPlay2Build <- totalPlay2Build + reactionTime
-      play2BuildCounter <- play2BuildCounter + 1
+  
+  avgTotalTransferTime = totalTime/counter
+  avgView2Play = totalView2Play/view2PlayCounter
+  avgBuild2Play = totalBuild2Play/build2PlayCounter
+  avgPlay2Build = totalPlay2Build/play2BuildCounter
+  avgPlay2View = totalPlay2View/play2ViewCounter
+  
+  
+  
+  # newPartData <- data.frame(Participant = factor(),
+  #                           Age = numeric(),
+  #                           Gender = factor(),
+  #                           AvgTotalTransferTime = numeric(),
+  #                           AvgView2Play = numeric(),
+  #                           AvgBuild2Play = numeric(),
+  #                           AvgPlay2Build = numeric(),
+  #                           AvgPlay2View = numeric(),
+  #                           group = factor(),
+  #                           condition = factor(),
+  #                           stringsAsFactors = FALSE)
 
-    }else{
-      print("nothin bb")
-    }
-  }
-  
-  if(currentEvent == playWall){
-    if(previousEvent == viewWall){
-      
-      totalPlayWallCount <- totalPlayWallCount + 1
-      totalTime <- totalTime + reactionTime 
-      totalView2Play <- totalView2Play + reactionTime
-      view2PlayCounter <- view2PlayCounter + 1
-      
-    }else if(previousEvent == buildWall){
-      
-      totalPlayWallCount <- totalPlayWallCount + 1
-      totalTime <- totalTime + reactionTime 
-      totalBuild2Play <- totalBuild2Play + reactionTime
-      build2PlayCounter <- build2PlayCounter + 1
-      
-    }else{
-      print("nothin bb")
-    }
-  }
-  previousEvent <- currentEvent
-  previousTime <- soloDFshort[i,1]/10000
-  
-  
+  newPartRow <- data.frame(shortGroupDF[5,2], shortGroupDF[5,5], shortGroupDF[5,6], avgTotalTransferTime, avgView2Play, avgBuild2Play, avgPlay2Build, avgPlay2View, partCondition, partGroup)
+  newPartData <- rbind(newPartData, newPartRow)
+
+  write.csv(newPartData, "AllSubjectGazeData.csv")
+
 }
-
-
-avgTotalTransferTime = totalTime/counter
-avgView2Play = totalView2Play/view2PlayCounter
-avgBuild2Play = totalBuild2Play/build2PlayCounter
-avgPlay2Build = totalPlay2Build/play2BuildCounter
-avgPlay2View = totalPlay2View/play2BuildCounter
 
