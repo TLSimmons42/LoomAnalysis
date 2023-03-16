d3f31095338e9e5f86942c4b78723039747c69f2
TLSimmons42 <TrentNeuro42@gmail.com>
2023-03-16 02:30:21 -0400

    stuff

--- GazeData/SfNGazePlots.R
+++ GazeData/SfNGazePlots.R
@@ -7,6 +7,7 @@ library(ggtext)
 library(ggsci)
 library(ggpubr)
 library(ez)
+library(ggpubr)
 
 
 
@@ -64,7 +65,7 @@ boxplot(use)
 #avg total Gaze Transfer Time analysis
 gazePlot <- df%>%
   group_by(condition, group)%>%
-  summarise(mATT = mean(pacStay), sATT = sd(pacStay))%>%
+  summarise(mATT = mean(avgBuild2Play), sATT = sd(avgBuild2Play))%>%
   ggplot(aes(reorder(condition,mATT),mATT, fill = reorder(group,mATT)))+
   geom_bar(stat = "identity", position = "dodge")+
   #geom_text(mapping=aes(label=round(mATT,2)), position = position_dodge(width = 0.9),
@@ -112,14 +113,48 @@ dat.avg <-
   dplyr::mutate(Group = as.factor(group) %>%
                   fct_recode(ASD="e", Ctrl="c"))
 
-dat.avg %>%
-  ggplot(aes(x=Condition, y= pacStay, color=Group)) +
+v <- dat.avg %>%
+  ggplot(aes(x=Condition, y= avgViewFix, color=Group, fill=Group)) +
   geom_boxplot() +
   scale_color_brewer(palette = "Set1") +
   xlab("") +
-  ylab("Grasp Duration (ms)") +
-  theme_bw()
+  ylab("View Wall Fixation Time (ms)") +
+  ylim(0, 2250)+
+  theme_pubr()
 
-ezANOVA(dat.avg, dv = pacStay, wid = Participant, 
+ggsave("View Wall .PNG", width = 9, height = 6, units = "cm", dpi = 320 )  
+
+p <- dat.avg %>%
+  ggplot(aes(x=Condition, y= avgPlayFix, color=Group, fill=Group)) +
+  geom_boxplot() +
+  scale_color_brewer(palette = "Set1") +
+  xlab("") +
+  ylab("Play Wall Fixation Time (ms)") +
+  ylim(0, 2250)+
+  theme_pubr()
+ggsave("Play Wall Fixation.PNG", width = 9, height = 6, units = "cm", dpi = 320 )  
+
+
+b <- dat.avg %>%
+  ggplot(aes(x=Condition, y= avgBuildFix, color=Group, fill=Group)) +
+  geom_boxplot() +
+  scale_color_brewer(palette = "Set1") +
+  xlab("") +
+  ylab("Build Wall Fixation Time (ms)") +
+  ylim(0, 2250)+
+  theme_pubr()
+
+ggsave("Build Wall Fixation.PNG", width = 9, height = 6, units = "cm", dpi = 320 )  
+
+
+
+
+ggarrange(v,p,b,
+          labels = c("View Wall Fixation Time (ms)", "Play Wall Fixation Time (ms)", "Build Wall Fixation Time (ms)"),
+          ncol(3), nrow(1))
+
+ggsave("Gaze Durration Figure.PNG", width = 15, height = 8, units = "cm", dpi = 320 )  
+
+ezANOVA(dat.avg, dv = avgPlayFix, wid = Participant, 
         within = c(Condition), between = c(Group), type=2)
 
