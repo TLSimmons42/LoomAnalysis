ac2b66401b6220e38367b081dbbdc67cb4a2493d
TLSimmons42 <80668297+TLSimmons42@users.noreply.github.com>
2023-08-07 09:20:26 -0700

    stuff

--- loom 2.0/Loom2SinglePersonanalysis.R
+++ loom 2.0/Loom2SinglePersonanalysis.R
@@ -3,21 +3,238 @@ library(rgl)
 library(dplyr)
 library(bit64)
 
-participantDataFile <- "loomtest1 (1).csv"
-
-df <- read.csv(participantDataFile, colClasses=c("TimeStamp" = "integer64"), header = TRUE, sep = ",", stringsAsFactors = FALSE)
 
 
-#shortDF <- df[(df$CurrentGazeArea == "play_wall" | df$CurrentGazeArea == "build_wall" | df$CurrentGazeArea == "view_wall") & df$CurrentGazeTarget == "none", ]
-#shortDF <- df[(df$CurrentGazeArea == "play_wall" | df$CurrentGazeArea == "build_wall" | df$CurrentGazeArea == "view_wall"), ]
-shortDF <- df[df$CurrentGazeArea == "play_wall"& df$CurrentGazeTarget == "none",]
+#participantDataFile <- "sdP1_old1.csv"
 
-# x <- df[,13]
-# y <- df[,14]
-# z <- df[,15]
+data_files <- list.files(pattern = "sdP")
+data_files[]
 
-x <- shortDF[,13]
-y <- shortDF[,14]
-z <- shortDF[,15]
+gazeDurrationTimes <- data.frame(Time = numeric(),
+                                 Participant = factor(),
+                                 Condition = factor(),
+                                 Trial = numeric(),
+                                 avgPlayWallDurration = numeric(),
+                                 avgViewWAllDurration = numeric(),
+                                 avgBuildWallDurration = numeric(),
+                                 playWallCounter = numeric(),
+                                 viewWallCounter = numeric(),
+                                 buildWallCounter = numeric(),
+                                 playWallTotalTime = numeric(),
+                                 viewWallTotalTime = numeric(),
+                                 buildWallTotalTime = numeric(),
+                                 stringsAsFactors = FALSE)
 
-plot3d(x, y, z)
+
+durationEventDF <- data.frame(Participant = factor(),
+                                 Condition = factor(),
+                                 Trial = numeric(),
+                                 Event = factor(),
+                                 viewSwitchCounter = numeric(),
+                                 startTime = numeric(),
+                                 endTime = numeric(),
+                                 eventDuration = numeric(),
+                                 stringsAsFactors = FALSE)
+
+
+
+for(f in 1:length(data_files))
+{
+  participantDataFile <- data_files[f]
+  print(participantDataFile)
+  
+  
+  
+  df <- read.csv(participantDataFile, colClasses=c("Time" = "integer64"), header = TRUE, sep = ",", stringsAsFactors = FALSE)
+  df <- df[!duplicated(df$Time), ]
+  
+  
+  #shortDF <- df[(df$CurrentGazeArea == "play_wall" | df$CurrentGazeArea == "build_wall" | df$CurrentGazeArea == "view_wall") & df$CurrentGazeTarget == "none", ]
+  #shortDF <- df[(df$CurrentGazeArea == "play_wall" | df$CurrentGazeArea == "build_wall" | df$CurrentGazeArea == "view_wall"), ]
+  #shortDF <- df[df$CurrentGazeArea == "play_wall"& df$CurrentGazeTarget == "none",]
+  
+  #shortDF <- df[(df$CurrentGazeArea == "play_wall" |df$CurrentGazeArea == "background_wall" | df$CurrentGazeArea == "view_wall"| df$CurrentGazeArea == "build_wall"), ]
+  
+  df$EyePos_X <- as.numeric(df$EyePos_X)
+  #shortDF <- df[(df$CurrentGazeArea == "play_wall" & df$EyePos_X > (0)),]
+  trimedDF <- df %>% filter((round(EyePos_X, 2) > -5 & CurrentGazeArea == "play_wall" & round(EyePos_X, 2) < 2.5 ) | CurrentGazeArea == "build_wall"| CurrentGazeArea == "view_wall" | CurrentGazeArea == "background_wall")
+  
+  # x <- df[,13]
+  # y <- df[,14]
+  # z <- df[,15]
+  
+  x <- trimedDF[,13]
+  y <- trimedDF[,14]
+  z <- trimedDF[,15]
+  
+  
+  plot3d(x, y, z)
+  
+  
+  #-------------------------------------------------------------------------------------------
+  # Gaze Durration Analysis
+  
+  
+  lookingForNewGazeDurration = TRUE
+  lookingForViewWall = FALSE
+  lookingForPlayWall = FALSE
+  lookingForBuildWall = FALSE
+  durationCounter <- 0
+  firstRowTime <- 0
+  
+  playWallTotalTime <- 0
+  buildWallTotalTime <- 0
+  viewWallTotalTime <- 0
+  
+  playWallCounter <- 0
+  buildWallCounter <- 0 
+  viewWallCounter <- 0
+
+  startTime <- 0
+  endTime <- 0
+  viewSwitchCounter <- 0
+  
+  
+  
+  
+  for (i in 2:nrow(trimedDF))
+  {
+    currentEvent <- trimedDF[i,12]
+    currentTime <- trimedDF[i,1]
+    currentEvent <- toString(currentEvent)
+    
+    if(lookingForNewGazeDurration)
+    {
+      if(currentEvent == "view_wall")
+      {
+        firstRowTime <- currentTime
+        lookingForViewWall = TRUE
+        lookingForNewGazeDurration = FALSE
+      }
+      if(currentEvent == "play_wall")
+      {
+        firstRowTime <- currentTime
+        lookingForPlayWall = TRUE
+        lookingForNewGazeDurration = FALSE
+      }
+      if(currentEvent == "build_wall")
+      {
+        firstRowTime <- currentTime
+        lookingForBuildWall = TRUE
+        lookingForNewGazeDurration = FALSE
+      }
+      if(currentEvent == "background_wall")
+      {
+        lookingForNewGazeDurration = TRUE
+      }
+    }
+    
+    if(!lookingForNewGazeDurration)
+    {
+      if(lookingForViewWall)
+      {
+        if(currentEvent == "view_wall")
+        {
+          durationCounter <- durationCounter +1
+          
+        }else{
+          lookingForNewGazeDurration = TRUE
+          lookingForViewWall = FALSE
+          
+          if(durationCounter>1){
+            viewWallCounter <- viewWallCounter +1
+            newTime <- currentTime - firstRowTime
+            viewWallTotalTime <- viewWallTotalTime +newTime
+            AddRow(firstRowTime, currentTime, newTime, viewSwitchCounter, "view_wall", durationEventDF, trimedDF)
+            
+          }
+        }
+      }
+      if(lookingForPlayWall)
+      {
+        if(currentEvent == "play_wall")
+        {
+          durationCounter <- durationCounter +1
+          
+        }else{
+          lookingForNewGazeDurration = TRUE
+          lookingForPlayWall = FALSE
+          
+          if(durationCounter>1){
+            playWallCounter <- playWallCounter +1
+            newTime <- currentTime - firstRowTime
+            playWallTotalTime <- playWallTotalTime +newTime
+            AddRow(firstRowTime, currentTime, newTime, viewSwitchCounter,"play_wall", durationEventDF, trimedDF)
+            
+          }
+        }
+      }
+      
+      if(lookingForBuildWall)
+      {
+        if(currentEvent == "build_wall")
+        {
+          durationCounter <- durationCounter +1
+          
+        }else{
+          lookingForNewGazeDurration = TRUE
+          lookingForBuildWall = FALSE
+          
+          if(durationCounter>1){
+            buildWallCounter <- buildWallCounter +1
+            newTime <- currentTime - firstRowTime
+            buildWallTotalTime <- buildWallTotalTime +newTime
+            AddRow(firstRowTime, currentTime, newTime, viewSwitchCounter,"build_wall", durationEventDF, trimedDF)
+          }
+        }
+      }
+    }
+  }
+  
+  avgPlayWallDurration <- playWallTotalTime/playWallCounter/10000
+  avgViewWAllDurration <- viewWallTotalTime/viewWallCounter/10000
+  avgBuildWallDurration <- buildWallTotalTime/buildWallCounter/10000
+  
+  
+  # print(playWallTotalTime)
+  # print(viewWallTotalTime)
+  # print(buildWallTotalTime)
+  # 
+  # print(playWallCounter)
+  # print(viewWallCounter)
+  # print(buildWallCounter)
+  # 
+  # print(avgPlayWallDurration)
+  # print(avgViewWAllDurration)
+  # print(avgBuildWallDurration)
+  
+  
+  Participant <- trimedDF[2,2]
+  Condition <- trimedDF[8,8]
+  Trial <- trimedDF[9,9]
+  
+  
+  newPartRow <- data.frame(Participant, Condition, Trial, avgPlayWallDurration, avgViewWAllDurration, 
+                           avgBuildWallDurration, playWallCounter, viewWallCounter, buildWallCounter, playWallTotalTime, viewWallTotalTime, buildWallTotalTime)
+  
+  gazeDurrationTimes <- rbind(gazeDurrationTimes, newPartRow)
+
+}
+
+  
+AddRow <- function(startTime, endTime, eventDuration, counter, Event, df, df2){
+  Participant <- df2[2,2]
+  Condition <- df2[8,8]
+  Trial <- df2[9,9]
+  viewSwitchCounter <<- viewSwitchCounter + 1
+
+  
+  
+  newRow <- data.frame(Participant, Condition, Trial, Event, viewSwitchCounter, startTime, endTime, eventDuration)
+  durationEventDF <<- rbind(durationEventDF, newRow)
+}
+
+
+
+
+
