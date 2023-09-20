bff2b984ed7724303cb38b720646911f48caa469
TLSimmons42 <TrentNeuro42@gmail.com>
2023-09-19 22:23:16 -0400

    fsdf

--- loom 2.0/All DATA Folder/Analysis Scripts/PACdataCleaning.R
+++ loom 2.0/All DATA Folder/Analysis Scripts/PACdataCleaning.R
@@ -6,7 +6,7 @@ library(stringr)
 
 
 
-data_files <- list.files(pattern = "P10_old6")
+data_files <- list.files(pattern = "P10_old1")
 
 PACdf <- data.frame(Time = numeric(),
                    Participant = factor(),
@@ -29,6 +29,18 @@ individualPACdf <- data.frame(Time = numeric(),
                     Event = factor(),
                     stringsAsFactors = FALSE)
 
+individualAreaPACdf <- data.frame(Time = numeric(),
+                              Participant = factor(),
+                              Condition = factor(),
+                              Trial = numeric(),
+                              PACtype = factor(),
+                              PACtime = numeric(),
+                              PACstartTime = numeric(),
+                              PACendTime = numeric(),
+                              Event = factor(),
+                              stringsAsFactors = FALSE)
+
+
 
 for(f in 1:length(data_files))
 {
@@ -37,8 +49,11 @@ for(f in 1:length(data_files))
   
   df <- read.csv(participantDataFile, colClasses=c("Time" = "integer64"), header = TRUE, sep = ",", stringsAsFactors = FALSE)
   df <- df[!duplicated(df$Time), ]
-  
+  df$Event <- gsub("\\(|\\)", "", df$Event)
+  df$CurrentGazeTarget <- gsub("\\(|\\)", "", df$CurrentGazeTarget)
 
+
+  
   # Find all of the Picked events
   trimedGrabDF <- df[grep("picked", df$Event), ]
   trimedGrabDF <- trimedGrabDF %>% filter(CurrentGazeArea == "play_wall")
@@ -52,8 +67,10 @@ for(f in 1:length(data_files))
   trimedPlaceDF <- trimedPlaceDF %>% filter(!grepl("Hit", Event))
   trimedPlaceDF <- trimedPlaceDF %>% filter(!grepl("HB", Event))
   trimedPlaceDF <- trimedPlaceDF[!duplicated(trimedPlaceDF$Event), ]
+  #trimedPlaceDF <- trimedPlaceDF %>% filter(CurrentGazeArea == "build_wall")
+  print("hello1")
   
-  for(b in 1:length(trimedP2GrabDF))
+  for(b in 1:nrow(trimedP2GrabDF))
   {
     input_string <- trimedP2GrabDF[b,10]
     
@@ -63,10 +80,9 @@ for(f in 1:length(data_files))
     # Extract the substring before the target text
     result <- substr(input_string, 1, pos - 1)
     result <- paste(result, "was", sep = "")
-    result <- gsub("(Clone)", "Clone", result)
+    #result <- gsub("(Clone)", "Clone", result)
     
-    print(result)
-    
+
     trimedPlaceDF <- trimedPlaceDF %>% filter(!grepl(result, Event))
     
   }
@@ -89,9 +105,10 @@ for(f in 1:length(data_files))
   PACstartTime <- 0
   PACendTime <- 0
   
+  print("hello2")
   
   
-  for(i in 1:length(trimedGrabDF))
+  for(i in 1:nrow(trimedGrabDF))
   {
     currentTime <- trimedGrabDF[i,1]
     subDF <- df %>% filter(Time < (20000000+currentTime) & Time > (currentTime-20000000) )
@@ -104,13 +121,13 @@ for(f in 1:length(data_files))
     input_string <- trimedGrabDF[i,10]
     
     # Find the position of " was picked up"
-    pos <- regexpr("was picked up", input_string)
+    pos <- regexpr(" was picked up", input_string)
     
     # Extract the substring before the target text
     result <- substr(input_string, 1, pos - 1)
     
     # Print the result
-
+    #print(result)
 
     first_instance <- which(subDF$CurrentGazeTarget == result)[1]
     
@@ -138,15 +155,34 @@ for(f in 1:length(data_files))
   }
   
   
-  for(i in 1:length(trimedPlaceDF))
+  for(i in 1:nrow(trimedPlaceDF))
   {
     currentTime <- trimedPlaceDF[i,1]
-    subDF1 <- df %>% filter(Time < (20000000+currentTime) & Time > (currentTime-20000000) )
+    currentGazeArea <- trimedPlaceDF[i,12]
+    currentEvent <- trimedPlaceDF[i,10]
+    
+    subDF1 <- df %>% filter(Time > (currentTime-20000000) & Time <= currentTime)
     
     Participant <- trimedPlaceDF[2,2]
     Condition <- trimedPlaceDF[8,8]
     Trial <- trimedPlaceDF[9,9]
     
+/
+    
+    
+    # dfLength <- length(subDF1)
+    # areaCheck <- subDF1[dfLength,12]
+    # print(areaCheck)
+    # while(areaCheck == currentGazeArea)
+    # {
+    #   dfLength <- dfLength - 1
+    #   areaCheck <- subDF1[dfLength,12]
+    #   print(dfLength)
+    #   print(subDF1[dfLength,10])
+    #   #print(areaCheck)
+    # }
+    # 
+    # print("hmmmmmmmmm")
     
     input_string <- trimedPlaceDF[i,10]
     
