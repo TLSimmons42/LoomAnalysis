ac58111459d3e354616d02039ade516c80f9fd7a
TLSimmons42 <TrentNeuro42@gmail.com>
2023-11-19 23:30:31 -0500

    stuff

---
+++ GazeData/SfNGazePlots.R
@@ -0,0 +1,328 @@
+library(plot3D)
+library(rgl)
+#library(dplyr)
+library(ggplot2)
+library(tidyverse)
+library(ggtext)
+library(ggsci)
+library(ggpubr)
+library(ez)
+library(ggpubr)
+library(cowplot)
+
+
+#dataFile <- "AllSubjectGazeData2-25-23_OneMin.csv"
+#dataFile <- "pacMoving_2-25-23_OneMin.csv"
+
+#dataFile <- "AllSubjectGazeData3-11-23_OneMin.csv"
+
+
+
+
+df <- read.csv(dataFile, header = TRUE, sep = ",", stringsAsFactors = FALSE)
+
+df$group[df$group == "e"] <- 'AUT'
+df$group[df$group == "c"] <- 'Non-AUT'
+
+df$condition[df$condition == "s"] <- 'Solo'
+df$condition[df$condition == "co"] <- 'Cooperative'
+
+soloDF <- df[df$condition == "Solo",]
+coDF <- df[df$condition == "Cooperative",]
+
+cDF <- df[df$group == "Non-AUT",]
+eDF <- df[df$group == "AUT",]
+
+CsoloDF <- df[df$condition == "Solo" & df$group == "Non-AUT",]
+EsoloDF <- df[df$condition == "Solo" & df$group == "AUT",]
+
+CcoDF <- df[df$condition == "Cooperative" & df$group == "Non-AUT",]
+EcoDF <- df[df$condition == "Cooperative" & df$group == "AUT",]
+
+
+# #
+# df$group[df$partGroup == "e"] <- 'AUT'
+# df$group[df$partGroup == "c"] <- 'Non-AUT'
+# 
+# df$condition[df$condition == "s"] <- 'Solo'
+# df$condition[df$condition == "co"] <- 'Cooperative'
+# 
+# soloDF <- df[df$condition == "s",]
+# coDF <- df[df$condition == "co",]
+# 
+# cDF <- df[df$group == "c",]
+# eDF <- df[df$group == "e",]
+
+
+
+
+#-------------------------------------------------------------------------------------------------------------------------------
+#View wall analysis
+use <- df$pacMove
+sd(use)
+boxplot(use)
+
+#-------------------------------------------------------------------------------------------------------------------------------
+#avg total Gaze Transfer Time analysis
+gazePlot <- df%>%
+  group_by(condition, group)%>%
+  summarise(mATT = mean(avgBuild2Play), sATT = sd(avgBuild2Play))%>%
+  ggplot(aes(reorder(condition,mATT),mATT, fill = reorder(group,mATT)))+
+  geom_bar(stat = "identity", position = "dodge")+
+  #geom_text(mapping=aes(label=round(mATT,2)), position = position_dodge(width = 0.9),
+  #        cex= 2.5, vjust=-2)+
+  labs(title = "PAC Cube Placement Sequence",
+       subtitle = "",
+       x = "Trial Condition", y = "Time (ms)",
+       #caption = "moo",
+       fill = "")+
+  geom_errorbar(mapping = aes(ymin = mATT-sATT, ymax = mATT + sATT),
+                width = 0.2, position = position_dodge(width = 0.9))+
+  theme_pubclean()+scale_fill_startrek() 
+
+gazePlot
+
+#------------------------------------------------------------------------------------------
+
+gazePlot <- df%>%
+  group_by(condition, group)%>%
+  summarise(mATT = mean(avgPlay2View), sATT = sd(avgPlay2View))%>%
+  ggplot(aes(reorder(condition,mATT),mATT, fill = reorder(group,mATT)))+
+  geom_bar(stat = "identity", position = "dodge")+
+  #geom_text(mapping=aes(label=round(mATT,2)), position = position_dodge(width = 0.9),
+  #          cex= 2.5, vjust=-2)+
+  labs(title = "",
+       #subtitle = "kitty",
+       x = "", y = "",
+       #caption = "moo",
+       fill = "")+
+  geom_errorbar(mapping = aes(ymin = mATT-sATT, ymax = mATT + sATT),
+                width = 0.2, position = position_dodge(width = 0.9))+
+  theme_pubclean()+scale_fill_startrek() 
+
+
+gazePlot
+
+#-------------------------------------------------------------------------------------------------------------------------------
+#TESTING CODE
+#read.table("pacMoving_2-25-23_OneMin.csv",
+
+getViewWall <- function(){
+  dat.avg <-
+    read.table("AllSubjectGazeData3-11-23_OneMin.csv",
+               header=T, sep=",") %>%
+    dplyr::mutate(Participant = as.factor(Participant)) %>%
+    dplyr::mutate(Condition = factor(condition, levels=c("s", "co")) %>%
+                    fct_recode(Solo="s", Cooperative="co")) %>%
+    dplyr::mutate(Group = as.factor(group) %>%
+                    fct_recode(Autistic="e", "Non-Autistic"="c"))
+  
+  
+  dat.avg %>%
+    ggplot(aes(x=Condition, y= avgViewFix, color=Group, fill=Group)) +
+    geom_boxplot() +
+    scale_color_brewer(palette = "Set1") +
+    xlab("View Wall") +
+    ylab("Fixation Time (ms)") +
+    ylim(0, 2250)+
+    theme_pubr()+ 
+    theme(axis.title.x = element_text(face="bold"))
+ 
+}  
+getPlayWall <- function(){
+  dat.avg <-
+    read.table("AllSubjectGazeData3-11-23_OneMin.csv",
+               header=T, sep=",") %>%
+    dplyr::mutate(Participant = as.factor(Participant)) %>%
+    dplyr::mutate(Condition = factor(condition, levels=c("s", "co")) %>%
+                    fct_recode(Solo="s", Cooperative="co")) %>%
+    dplyr::mutate(Group = as.factor(group) %>%
+                    fct_recode(Autistic="e", "Non-Autistic"="c"))
+  
+  
+  dat.avg %>%
+    ggplot(aes(x=Condition, y= avgPlayFix, color=Group, fill=Group)) +
+    geom_boxplot() +
+    scale_color_brewer(palette = "Set1") +
+    xlab("Play Wall") +
+    ylab("Play Wall Fixation Time (ms)") +
+    ylim(0, 2250)+
+    theme_pubr()+
+    theme(axis.title.x = element_text(face="bold"))
+  
+}  
+
+getBuildWall <- function(){
+  dat.avg <-
+    read.table("AllSubjectGazeData3-11-23_OneMin.csv",
+               header=T, sep=",") %>%
+    dplyr::mutate(Participant = as.factor(Participant)) %>%
+    dplyr::mutate(Condition = factor(condition, levels=c("s", "co")) %>%
+                    fct_recode(Solo="s", Cooperative="co")) %>%
+    dplyr::mutate(Group = as.factor(group) %>%
+                    fct_recode(Autistic="e", "Non-Autistic"="c"))
+  
+  
+  dat.avg %>%
+    ggplot(aes(x=Condition, y= avgBuildFix, color=Group, fill=Group)) +
+    geom_boxplot() +
+    scale_color_brewer(palette = "Set1") +
+    xlab("Build Wall") +
+    ylab("Build Wall Fixation Time (ms)") +
+    ylim(0, 2250)+
+    theme_pubr()+
+    theme(axis.title.x = element_text(face="bold"))
+  
+}  
+
+
+v <- getViewWall() 
+p <- getPlayWall()+ ylab(NULL)
+b <- getBuildWall() + ylab(NULL)
+
+
+prow <- plot_grid(
+  v + theme(legend.position = "none"),
+  p + theme(legend.position = "none"),
+  b + theme(legend.position = "none"),
+  labels = c("A", "B", "C"),
+  nrow = 1, ncol = 3)
+
+
+
+legend <- get_legend(
+  b + theme(legend.position = "right")
+  )
+plot_grid(prow, legend,  rel_widths = c(4,.7)
+)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+cowplot::
+ggarrange(v,p,b,
+          labels = c("View Wall Fixation Time (ms)", "Play Wall Fixation Time (ms)", "Build Wall Fixation Time (ms)"),
+          ncol(3), nrow(1))
+
+ggsave("Gaze Durration Figure.PNG", width = 15, height = 8, units = "cm", dpi = 320 )  
+
+
+PACmove <- dat.avg %>%
+  ggplot(aes(x=Condition, y= pacMove, color=Group, fill=Group)) +
+  geom_boxplot() +
+  scale_color_brewer(palette = "Set1") +
+  xlab("") +
+  ylab("Cube Grabbing Sequence") +
+  ylim(0, 850)+
+  theme_pubr()
+PACmove
+
+PACstay <- dat.avg %>%
+  ggplot(aes(x=Condition, y= pacStay, color=Group, fill=Group)) +
+  geom_boxplot() +
+  scale_color_brewer(palette = "Set1") +
+  xlab("") +
+  ylab("Cube Placement Sequenc") +
+  ylim(0, 850)+
+  theme_pubr()
+PACstay
+
+
+ggsave("View Wall.PNG", width = 9, height = 6, units = "cm", dpi = 320 )  
+
+p <- dat.avg %>%
+  ggplot(aes(x=Condition, y= avgPlayFix, color=Group, fill=Group)) +
+  geom_boxplot() +
+  scale_color_brewer(palette = "Set1") +
+  xlab("") +
+  ylab("Play Wall Fixation Time (ms)") +
+  ylim(0, 2250)+
+  theme_pubr()
+ggsave("Play Wall Fixation.PNG", width = 9, height = 6, units = "cm", dpi = 320 )  
+p
+
+
+
+#-----------------------------------------------------------------------------------------------------------
+
+dat.avg <-
+  read.table("coDF.csv",
+             header=T, sep=",") %>%
+  dplyr::mutate(Participant = as.factor(Participant)) %>%
+  dplyr::mutate(Condition = factor(condition, levels=c("s", "Cooperative")) %>%
+                  fct_recode(Solo="s", Cooperative="Cooperative")) %>%
+  dplyr::mutate(Group = as.factor(group) %>%
+                  fct_recode(Autistic="AUT", "Non-Autistic"="Non-AUT"))
+
+ezANOVA(dat.avg, dv = avgViewFix, wid = Participant, 
+        between = c(Group), type=2)
+#-----------------------------------------------------------------------------------------------------------
+
+dat.avg <-
+  read.table("solooDF.csv",
+             header=T, sep=",") %>%
+  dplyr::mutate(Participant = as.factor(Participant)) %>%
+  dplyr::mutate(Condition = factor(condition, levels=c("Solo", "Cooperative")) %>%
+                  fct_recode(Solo="Solo", Cooperative="Cooperative")) %>%
+  dplyr::mutate(Group = as.factor(group) %>%
+                  fct_recode(Autistic="AUT", "Non-Autistic"="Non-AUT"))
+
+ezANOVA(dat.avg, dv = avgViewFix, wid = Participant, 
+        between = c(Group), type=2)
+
+#-----------------------------------------------------------------------------------------------------------
+
+
+dat.avg <-
+  read.table("AllSubjectGazeData3-11-23_OneMin.csv",
+             header=T, sep=",") %>%
+  dplyr::mutate(Participant = as.factor(Participant)) %>%
+  dplyr::mutate(Condition = factor(condition, levels=c("s", "co")) %>%
+                  fct_recode(Solo="s", Cooperative="co")) %>%
+  dplyr::mutate(Group = as.factor(group) %>%
+                  fct_recode(Autistic="e", "Non-Autistic"="c"))
+
+ezANOVA(dat.avg, dv = avgViewFix, wid = Participant, 
+        within = c(Condition), between = c(Group), type=2)
+
+#-----------------------------------------------------------------------------------------------------------
+
+dat.avg <-
+  read.table("pacMoving_2-25-23_OneMin.csv",
+             header=T, sep=",") %>%
+  dplyr::mutate(Participant = as.factor(Participant)) %>%
+  dplyr::mutate(Condition = factor(condition, levels=c("s", "co")) %>%
+                  fct_recode(Solo="s", Cooperative="co")) %>%
+  dplyr::mutate(Group = as.factor(partGroup) %>%
+                  fct_recode(Autistic="e", "Non-Autistic"="c"))
+
+ezANOVA(dat.avg, dv = pacStay, wid = Participant, 
+        within = c(Condition), between = c(Group), type=2)
+
+
+
+
+#-----------------------------------------------------------------------------------------------------------
+
+dat.avg <-
+  read.table("pacMoving_2-25-23_OneMin.csv",
+             header=T, sep=",") %>%
+  dplyr::mutate(Participant = as.factor(Participant)) %>%
+  dplyr::mutate(Condition = factor(condition, levels=c("s", "co")) %>%
+                  fct_recode(Solo="s", Cooperative="co")) %>%
+  dplyr::mutate(Group = as.factor(partGroup) %>%
+                  fct_recode(Autistic="e", "Non-Autistic"="c"))
+
+ezANOVA(dat.avg, dv = pacMove, wid = Participant, 
+        within = c(Condition), between = c(Group), type=2)
+
---
+++ Loom 1.0/Scripts/Analysis Scripts/TransferGazeAnalysis.R
@@ -0,0 +1,84 @@
+library(plot3D)
+library(rgl)
+library(dplyr)
+library(ggplot2)
+library(tidyverse)
+library(ggtext)
+library(ggpubr)
+library(ez)
+library(cowplot)
+library(ggsci)
+library(gridExtra)
+
+#dataFile <- "gazeTimesReal.csv"
+dataFile <- "singleGazeTransferDF.csv"
+
+
+df <- read.csv(dataFile, colClasses=c("TimeStamp" = "integer64"), header = TRUE, sep = ",", stringsAsFactors = FALSE)
+df <- read.csv(dataFile, header = TRUE, sep = ",", stringsAsFactors = FALSE)
+
+
+# for(i in 1:nrow(df))
+# {
+#   #if(df$Group[i] == "1" | df$Group[i] == "f"| is.na(df$Group[i]) | df$Participant[i] == "nuP20"| df$Participant[i] == "nuP17"){
+#   if(df$Group[i] == "1" | df$Group[i] == "f"| is.na(df$Group[i])){
+#     
+#     df$Group[i] <- "e"
+#   }
+#   if(df$Participant[i] == "sdP13"| df$Participant[i] == "sdP2"){
+#     df$Group[i] <- "c"
+#     
+#   }
+# }
+
+dfAut <- df %>% filter(df$Group == "e")
+dfNonAut <- df %>% filter(df$Group != "e")
+#df <- df %>% filter(df$Condition != "comp")
+#-------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+tempdf <- df %>% filter(!is.na(P2B))
+
+
+sd <-sd(tempdf$P2B)
+tempdf <- df %>% filter(P2B < sd*3)
+
+# 
+# tempdf <- tempdf %>% filter(avgPlayerDurration > 100)
+# tempdf <- tempdf %>% filter(Condition != "solo")
+#tempdf <- tempdf %>% filter(Group != "solo")
+#tempdf <- tempdf %>%   mutate(pointColor = ifelse(Group == "e", "orange", "purple"))
+
+standardPlot <- tempdf %>%
+  group_by(Condition, Group)%>%
+  mutate(pointColor = ifelse(Group == "c", "orange", "purple"))%>%
+  summarise(individualPoints = P2B,
+            pointColor = pointColor,
+            mATT = mean(P2B), sATT = sd(P2B),
+            CI_lower = mATT - 1.96 * sATT / sqrt(n()),
+            CI_upper = mATT + 1.96 * sATT / sqrt(n()))%>%
+  ggplot(aes(Condition,mATT, fill = reorder(Group,-mATT)))+
+  geom_bar(stat = "identity", position = "dodge")+
+  geom_point(aes(x = Condition, y = individualPoints, color = pointColor),
+             position = position_dodge(width = 1.2), size = 3) +
+  labs(title = "Partner Gaze Duration",
+       x = "", y = "Time (ms)",
+       #caption = "moo",
+       fill = "")+
+  #scale_y_continuous(limits = c(0, 1000))+  # Set y-axis limits
+  geom_errorbar(mapping = aes(ymin = CI_lower, ymax = CI_upper),
+                width = 0.2, position = position_dodge(width = 0.9), size = 1)+
+  theme_pubclean()+scale_fill_startrek()
+
+standardPlot <- standardPlot + scale_y_continuous(limits = c(0, 1750))
+standardPlot
+#standardPlot+ theme(legend.position = "none")
+
+#tempdf <- tempdf %>% filter(Group != "e")
+tempdf <- tempdf %>% filter(Condition == "s")
+t_test_result <- t.test(P2B ~ Group, data = tempdf , var.equal = FALSE)
+print(t_test_result)
+
+
+# tempdf <- tempdf %>% filter(Condition == "solo")
+# twoANOVA <- aov(tempdf$avgPlayWallDurration ~ factor(tempdf$Group) , data = tempdf)
+# summary(twoANOVA)
