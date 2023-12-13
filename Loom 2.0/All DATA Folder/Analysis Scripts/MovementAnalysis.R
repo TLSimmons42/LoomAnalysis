f7f21099089dfbe1212501bcd733abdf1ade6dfb
TLSimmons42 <80668297+TLSimmons42@users.noreply.github.com>
2023-12-13 17:26:17 -0500

    Some cool movement Data

--- Loom 2.0/All DATA Folder/Analysis Scripts/MovementAnalysis.R
+++ Loom 2.0/All DATA Folder/Analysis Scripts/MovementAnalysis.R
@@ -1,12 +1,18 @@
 library(plot3D)
 library(rgl)
 library(dplyr)
+library(ggplot2)
+library(tidyverse)
+library(ggtext)
+library(ggpubr)
+library(ez)
+library(cowplot)
+library(ggsci)
+library(gridExtra)
+library(ggsignif)
 library(bit64)
-library(stringr)
 
-
-
-data_files <- list.files(pattern = "sdP10")
+data_files <- list.files(pattern = "sdP10.csv")
 
 PACdf <- data.frame(Time = numeric(),
                     Participant = factor(),
@@ -68,11 +74,92 @@ individualAreaPACdf <- data.frame(Time = numeric(),
 
 
 
-for(f in 1:length(data_files))
-{
-  endHandX <- first_instance_row[1,25]
-  endHandY <- first_instance_row[1,26]
-  endHandY <- first_instance_row[1,27]
-  distance <- sqrt((endHandX - currentHandX)^2 + (endHandY - currentHandY)^2 + (endHandY - currentHandZ)^2)
-  velocity <- distance/PACtime
-}
+# for(f in 1:length(data_files))
+# {
+#   
+  participantDataFile <- data_files[1]
+  print(participantDataFile)
+  
+  
+  df <- read.csv(participantDataFile, colClasses=c("Time" = "integer64"), header = TRUE, sep = ",", stringsAsFactors = FALSE)
+  df <- df[!duplicated(df$Time), ]
+  
+  
+  # endHandX <- first_instance_row[1,25]
+  # endHandY <- first_instance_row[1,26]
+  # endHandY <- first_instance_row[1,27]
+  # distance <- sqrt((endHandX - currentHandX)^2 + (endHandY - currentHandY)^2 + (endHandY - currentHandZ)^2)
+  # velocity <- distance/PACtime
+  
+  trimDF <- df %>%
+    mutate(Time = (Time - Time[1])/10000000) 
+  
+  trimDF <- trimDF %>%
+    filter(Time <= 25)
+  xHand <- trimDF$HandPos_X
+  yHand <- trimDF$HandPos_Y
+  zHand <- trimDF$HandPos_Z
+  
+  trimDF <- trimDF %>%
+    mutate(ActionEvent = ifelse(grepl("picked", Event), Event, "none"))
+  
+  trimDF <- trimDF %>%
+    mutate(ActionEvent = ifelse(grepl("P2", ActionEvent), "none", ActionEvent))
+  
+  trimDF <- trimDF %>%
+    mutate(ActionEvent = ifelse(!grepl("P1", ActionEvent), ActionEvent, "none"))
+ 
+  trimDF <- trimDF %>%
+    mutate(ActionEvent = ifelse(ActionEvent != "none", "Grab", ActionEvent))
+ 
+  trimDF <- trimDF %>%
+    mutate(ActionEvent = ifelse(grepl("dropped", Event), Event, ActionEvent))
+  
+  trimDF <- trimDF %>%
+    mutate(ActionEvent = ifelse(grepl("P1", ActionEvent), "Dropped", ActionEvent))
+  
+  trimDF <- trimDF %>%
+    mutate(ActionEvent = ifelse(ActionEvent == "Grab" | ActionEvent == "Dropped", ActionEvent, "none"))
+
+  # trimDF <- trimDF %>%
+  #   mutate(ActionEvent = ifelse(ActionEvent != "none", "grab", ActionEvent))
+  # 
+  # 
+   
+  
+
+  
+  p <- trimDF %>%
+    ggplot(aes(x = Time, y = xHand, color = ActionEvent)) +
+    geom_line()+
+    geom_point(size = .05)+
+    # geom_line(aes(y = yHand), color = "red", linetype = "solid") +
+    # geom_line(aes(y = zHand), color = "green", linetype = "solid") +
+    labs(title = "Multiple Columns Line Plot", x = "X-axis label", y = "Y-axis label") +
+    theme_minimal()
+  
+  
+  #p + geom_point(aes(color = factor(ActionEvent)), size = 3)
+  # p + geom_point(aes (y = yHand),(color = factor(ActionEvent)), size = 3)
+  # p + geom_point(aes (y = zHand),(color = factor(ActionEvent)), size = 3)
+  
+  p
+  
+  
+  p <- trimDF %>%
+    ggplot(aes(x = Time)) +
+    geom_line(aes(y = xHand), color = "blue", linetype = "solid") +
+    # geom_line(aes(y = yHand), color = "red", linetype = "solid") +
+    # geom_line(aes(y = zHand), color = "green", linetype = "solid") +
+    labs(title = "Multiple Columns Line Plot", x = "X-axis label", y = "Y-axis label") +
+    theme_minimal()
+  
+  
+  #p + geom_point(aes(color = factor(ActionEvent)), size = 3)
+  # p + geom_point(aes (y = yHand),(color = factor(ActionEvent)), size = 3)
+  # p + geom_point(aes (y = zHand),(color = factor(ActionEvent)), size = 3)
+  
+  p
+  # plot3d(x, y, z)
+  
+# }
