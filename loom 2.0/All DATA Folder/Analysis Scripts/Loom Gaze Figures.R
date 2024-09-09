6bf4692bd81e0a9eee2fde3a187a2e0caf762f04
TLSimmons42 <80668297+TLSimmons42@users.noreply.github.com>
2024-09-09 14:45:25 -0400

    wowws

--- loom 2.0/All DATA Folder/Analysis Scripts/Loom Gaze Figures.R
+++ loom 2.0/All DATA Folder/Analysis Scripts/Loom Gaze Figures.R
@@ -12,7 +12,7 @@ library(cowplot)
 #dataFile <- "gazeDurationTimes 9-27-23.csv"
 
 #dataFileGaze <- "PACsfnREAL.csv"
-dataFile <- "GazeDurrationTimes 9_5_24.csv"
+dataFile <- "move and gaze times 9-8-24.csv"
 datFileGaze <- "GazeDurrationTimes 9_2_24.csv"
 
 gazeDF <- read.csv(datFileGaze, header = TRUE, sep = ",", stringsAsFactors = FALSE)
@@ -61,30 +61,59 @@ df <- df %>% mutate(Group = ifelse(Group == "f" & Participant == "sdP1", "e", Gr
 df <- df %>% mutate(Group = ifelse(Group == "e", "AUT", Group))
 df <- df %>% mutate(Group = ifelse(Group == "c", "Non-AUT", Group))
 
-subDF <- df %>% filter(Condition == "co")
 
-p <- df %>%
-  group_by(avgGrab, Group, Condition)%>%
-  summarise(mATT = mean(avgDrop), sATT = sd(avgDrop))%>%
-  ggplot(aes(Condition,mATT, fill = reorder(Group,mATT)))+
-  geom_boxplot() +
-  # geom_boxplot(outlier.shape = NA) +
-  labs(title = "Drop Movement Time", x = "", y = "Time (ms)")+
-  #theme(legend.position = "none")+
-  theme_bw()
-p <- p + guides(fill=guide_legend(title="Group"))
-p
+df <- df %>% mutate(Group = ifelse(Participant == "sdP3" , "AUT" , Group))
+df <- df %>% distinct(avgGrab, avgDrop, avgDropStart, .keep_all = TRUE)
+df <- df %>% mutate(Age = ifelse(Participant == "nuP10" & Sex == "f", 23 , Age))
+df <- df %>% mutate(Participant = ifelse(Participant == "p012" , "nuP12" , Participant))
+df <- df %>% mutate(Participant = ifelse(Participant == "P31" , "nuP31" , Participant))
+df <- df %>% mutate(Participant = ifelse(Participant == "Py" , "P7" , Participant))
+df <- df %>% mutate(Sex = ifelse(Participant == "nuP18" , "m" , Sex))
+df <- df %>% mutate(Age = ifelse(Participant == "nuP18" , 25 , Age))
+df <- df %>% mutate(Sex = ifelse(Participant == "nuP24" , "f" , Sex))
+df <- df %>% mutate(Participant = ifelse(Participant == "sd12" , "sdP12" , Participant))
+df <- df %>% mutate(Sex = ifelse(Participant == "sdP12" , "m" , Sex))
+df <- df %>% mutate(Sex = ifelse(Participant == "nuP19" , "f" , Sex))
+df <- df %>% mutate(Age = ifelse(Participant == "nuP24" , 24 , Age))
+df <- df %>% mutate(Age = ifelse(Participant == "sdP1" , 21 , Age))
+df <- df %>% mutate(Age = ifelse(Participant == "sdP13" , 20 , Age))
+df <- df %>% mutate(Sex = ifelse(Participant == "sdP15" , "m" , Sex))
+df <- df %>% mutate(Sex = ifelse(Participant == "sdP7" , "m" , Sex))
+df <- df %>% mutate(Participant = ifelse(Participant == "sdP27" , "nuP27" , Participant))
+
+df <- df %>% filter(Participant != "sdP18")
+df <- df %>% filter(!(Participant == "nuP10" & Age == 21))
+df <- df %>% filter(Participant != "nuP31")
+# df <- df %>% filter(Participant != "nuP30")
+# df <- df %>% filter(Participant != "nuP25")
+
+
+
+
+# p <- df %>%
+#   group_by(avgGrab, Group, Condition)%>%
+#   summarise(mATT = mean(avgDrop), sATT = sd(avgDrop))%>%
+#   ggplot(aes(Condition,mATT, fill = reorder(Group,mATT)))+
+#   geom_boxplot() +
+#   # geom_boxplot(outlier.shape = NA) +
+#   labs(title = "Drop Movement Time", x = "", y = "Time (ms)")+
+#   #theme(legend.position = "none")+
+#   theme_bw()
+# p <- p + guides(fill=guide_legend(title="Group"))
+# p
 #ggsave("Drop Time.pdf")
 
 
-df<- df %>% filter(!is.na(avgViewWAllDurration))
-df <- df %>% mutate(avgViewWAllDurration = avgViewWAllDurration / 1000)
+tempDF<- df %>% filter(!is.na(avgGrab))
+tempDF <- tempDF %>% filter(grabCount > 8)
+#tempDF <- tempDF %>% mutate(avgViewWAllDurration = avgViewWAllDurration / 1000)
+#tempDF <- tempDF %>% filter(Condition != "solo")
 
-p <- df %>%
+p <- tempDF %>%
   group_by(Group, Condition)%>%
   summarize(
-    Mean = mean(avgDropStart),
-    sd = sd(avgDropStart),
+    Mean = mean(avgGrab),
+    sd = sd(avgGrab),
     CI_lower = Mean - 1.96 * sd / sqrt(n()),
     CI_upper = Mean + 1.96 * sd / sqrt(n())) %>%
   ggplot(aes(Condition,Mean, fill = Group))+
@@ -96,7 +125,7 @@ p <- df %>%
 
 p <- p + guides(fill=guide_legend(title="Group"))
 p
-ggsave("View Wall Gaze Duration Time.pdf")
+#ggsave("View Builc Gaze Duration Time.pdf")
 
 # anova_model <- aov(Duration ~ Group, data = comboFigDF)
 # summary(anova_model)
@@ -151,3 +180,17 @@ ggplot(df, aes(x = Condition, y = avgDropStart, fill = reorder(Group,avgDropStar
 #   theme_pubclean()+scale_fill_startrek() 
 # 
 # gazePlot
+
+
+
+
+demoDFagg <- df %>% group_by(Participant, Group, Age, Sex) %>%
+  summarise(count = n())
+
+demoDFagg2 <- demoDFagg %>% group_by(Group, Sex) %>%
+  summarise(count = n())
+
+
+
+
+
